aa1d5c90c2e4cede3fc2393c91ae56e89c25489c
Anushka <shahanushka67@gmail.com>
2023-02-12 14:20:55 +0530

    Changes by anushka

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Anushka from './components/Anushka';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello there, this is a test project</h1>
+      <Anushka/>
     </div>
   );
 }
---
+++ src/components/Anushka.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Anushka() {
+  return (
+    <div>
+      Hey
+    </div>
+  )
+}
+
+export default Anushka
+Hey
