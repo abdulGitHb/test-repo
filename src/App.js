308e3d9918b091a04429e32a9b83cbdc7e7d88b2
Aditya Pandey <adityapandey272002@gmail.com>
2023-02-12 14:18:13 +0530

    aditya compnent added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Aditya from './aditya';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello there, this is a test project</h1>
+      <Aditya/>
     </div>
   );
 }
